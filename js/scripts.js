831cb134e5c0ab50ef2c8b0c6f0ff53b9b84a0db
Shawn Lunsford and Tanner Damron <tanner@>
2019-01-15 09:41:26 -0800

    add slideToggle to img and span class

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  // $(".clickable").click(function() {
+  //   $("#").toggle();
+  //   $("#").toggle();
+  // });
+  $(".clickable").click(function() {
+    $("#gopher-initial").slideToggle(1000);
+    $("#gopher-click").slideToggle(1000);
+  });
+});
